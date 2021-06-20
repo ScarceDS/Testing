281d131fda50b76b35e488e39c43ee7058fb80b2
ScarceDS <75695610+ScarceDS@users.noreply.github.com>
2021-06-21 00:10:47 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -6,6 +6,14 @@ import datetime
 import ta
 import pandas as pd
 import requests
+import os
+import plotly.graph_objects as go
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
+import yfinance as yf
+import numpy as np
+#from load_data import load_data
+import pandas_market_calendars as mcal
 #from pmdarima.arima import ADFTest
 #from pmdarima.arima import ndiffs
 #from pmdarima.arima import auto_arima
@@ -57,94 +65,55 @@ if Forecasting:
     
     n_periods= st.sidebar.slider('Forecasting period', min_value=1, max_value=5,
                              value=5,  step=1)
-    import matplotlib.pyplot as plt
-    #Splitting the data into testing and training
-    def test_train_split(data,test_split=0.2):
-        x=int((1-test_split)*len(data))
-        training_set = data.iloc[:x].values
-        test_set = data.iloc[x:].values
-        return training_set,test_set,x
-
-    # multi-step data preparation
-    from numpy import array
-
-    # split a univariate sequence into samples
-    def split_sequence(sequence, n_steps_in, n_steps_out):
-        X, y = list(), list()
-        for i in range(len(sequence)):
-            # find the end of this pattern
-            end_ix = i + n_steps_in
-            out_end_ix = end_ix + n_steps_out
-            # check if we are beyond the sequence
-            if out_end_ix > len(sequence):
-                break
-            # gather input and output parts of the pattern
-            seq_x, seq_y = sequence[i:end_ix], sequence[end_ix:out_end_ix]
-            X.append(seq_x)
-            y.append(seq_y)
-        return array(X), array(y)
-
-    from sklearn.preprocessing import MinMaxScaler
-    import pandas_market_calendars as mcal
-    df=data[price_type]
-    Train,Test,index=test_train_split(df,0.2)
-
-    sc = MinMaxScaler(feature_range = (0, 1))
-    # define input sequence
-    df1=Train.reshape(-1,1)
-    Raw_data =sc.fit_transform(df1)
-    # choose a number of time steps
-    n_steps_in, n_steps_out = 30, n_periods
-    # Prepare the new index
-    n_periods=n_steps_out
+    n_periods=5
+    #prepare the new index
     nyse=mcal.get_calendar('NYSE')
     new_index=nyse.valid_days(start_date=start, end_date='2030-01-01 00:00:00')
     new_index=new_index[0:(len(data)+n_periods)]
-    # split into samples
-    X, y = split_sequence(Raw_data, n_steps_in, n_steps_out)
-    # summarize the data
-    #for i in range(len(X)):
-       # print('\nX_train\n',X[i],'\n Y_train\n', y[i])
-    # reshape from [samples, timesteps] into [samples, timesteps, features]
-    n_features = 1
-    X = X.reshape((X.shape[0], X.shape[1], n_features))
 
-    # univariate multi-step vector-output stacked lstm example
-    from numpy import array
-    from keras.models import Sequential
-    from keras.layers import LSTM
-    from keras.layers import Dense
-    # define model
-    model1 = Sequential()
-    model1.add(LSTM(100, activation='relu', return_sequences=True, input_shape=(n_steps_in, n_features)))
-    model1.add(LSTM(100, activation='relu'))
-    model1.add(Dense(n_steps_out))
-    model1.compile(optimizer='adam', loss='mse')
-    #model1.summary()
-    # fit model
-    history1=model1.fit(X, y, epochs=50, verbose=0,validation_split=0.15)
-    #Testing
-    #Prepare Testing data
-    Test_data=Test[-n_steps_in:]
-    #print(Test_data.shape)
-    Test_data=Test_data.reshape(-1,1)
-    #print(Test_data.shape)
-    Test_data =sc.fit_transform(Test_data)
-    #print(Test_data.shape)
-    Test_data=Test_data.reshape((Test_data.shape[1],Test_data.shape[0], n_features))
-    #print(Test_data.shape)
-    ## Testing our model
-    yhat = model1.predict(Test_data, verbose=1)
-    #print('yhat shape',np.shape(yhat))
-    yhat=sc.inverse_transform(yhat)
-    yhat=yhat.reshape(-1,1)
-    #print(yhat)
-    New_df=pd.DataFrame(columns=['Close'],data=yhat,index=new_index[-n_periods:])
-    Old_df=pd.DataFrame(columns=['Close'],data=df.values,index=new_index[:-n_periods])
-    Final_df=pd.concat([Old_df,New_df],axis=0)
-    plt.plot(Final_df[-100:-n_periods+1])
-    plt.plot(Final_df[-n_periods:])
+    #Close Model
+    df_close=data['Close']
+    df_close.index.rename('ds',True)
+    df_close=df_close.reset_index()
+    df_close.columns=['ds','y']
+    model_close=Prophet()
+    model_close.fit(df_close)
+    future_dates_close=model_close.make_future_dataframe(periods=n_periods)
+    prediction_close=model_close.predict(future_dates_close)
+    #Open Model
+    df_open=data['Open']
+    df_open.index.rename('ds',True)
+    df_open=df_open.reset_index()
+    df_open.columns=['ds','y']
+    model_open=Prophet()
+    model_open.fit(df_open)
+    future_dates_open=model_open.make_future_dataframe(periods=n_periods)
+    prediction_open=model_open.predict(future_dates_open)
+    #High Model
+    df_high=data['High']
+    df_high.index.rename('ds',True)
+    df_high=df_high.reset_index()
+    df_high.columns=['ds','y']
+    model_high=Prophet()
+    model_high.fit(df_high)
+    future_dates_high=model_high.make_future_dataframe(periods=n_periods)
+    prediction_high=model_high.predict(future_dates_high)
+    #Low Model
+    df_low=data['Low']
+    df_low.index.rename('ds',True)
+    df_low=df_low.reset_index()
+    df_low.columns=['ds','y']
+    model_low=Prophet()
+    model_low.fit(df_low)
+    future_dates_low=model_low.make_future_dataframe(periods=n_periods)
+    prediction_low=model_low.predict(future_dates_low)
+    fig = go.Figure(data=[go.Candlestick(x=new_index,
+                open=prediction_open.yhat,
+                high=prediction_high.yhat,
+                low=prediction_low.yhat,
+                close=prediction_close.yhat)])
 
+    fig.show()
 #Simple Moving Average
 sma = st.sidebar.checkbox('Simple Moving Average')
 if sma:
