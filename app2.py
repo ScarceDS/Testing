ec464dda6196e603eea4d5c71d6c6de7c0f2826c
ScarceDS <75695610+ScarceDS@users.noreply.github.com>
2021-06-26 19:24:01 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -98,63 +98,65 @@ if Forecasting:
     n_periods= st.sidebar.slider('Forecasting period', min_value=1, max_value=30,
                                  value=5,  step=1)
     @st.cache(suppress_st_warning=True)
-    def forecast(data):
+    def forecast(data,price_type):
         
 
         #prepare the new index
         nyse=mcal.get_calendar('NYSE')
         new_index=nyse.valid_days(start_date=start, end_date='2030-01-01 00:00:00')
         new_index=new_index[0:(len(data)+n_periods)]
-
-        #Close Model
-        df_close=data['Close']
-        df_close.index.rename('ds',True)
-        df_close=df_close.reset_index()
-        df_close.columns=['ds','y']
-        model_close=Prophet()
-        model_close.fit(df_close)
-        future_dates_close=model_close.make_future_dataframe(periods=n_periods)
-        prediction_close=model_close.predict(future_dates_close)
-        #Open Model
-        df_open=data['Open']
-        df_open.index.rename('ds',True)
-        df_open=df_open.reset_index()
-        df_open.columns=['ds','y']
-        model_open=Prophet()
-        model_open.fit(df_open)
-        future_dates_open=model_open.make_future_dataframe(periods=n_periods)
-        prediction_open=model_open.predict(future_dates_open)
-        #High Model
-        df_high=data['High']
-        df_high.index.rename('ds',True)
-        df_high=df_high.reset_index()
-        df_high.columns=['ds','y']
-        model_high=Prophet()
-        model_high.fit(df_high)
-        future_dates_high=model_high.make_future_dataframe(periods=n_periods)
-        prediction_high=model_high.predict(future_dates_high)
-        #Low Model
-        df_low=data['Low']
-        df_low.index.rename('ds',True)
-        df_low=df_low.reset_index()
-        df_low.columns=['ds','y']
-        model_low=Prophet()
-        model_low.fit(df_low)
-        future_dates_low=model_low.make_future_dataframe(periods=n_periods)
-        prediction_low=model_low.predict(future_dates_low)
+        
+        #Prophet Model
+        df=data[price_type]
+        df.index.rename('ds',True)
+        df=df.reset_index()
+        df.columns=['ds','y']
+        model=Prophet()
+        model.fit(df)
+        future_dates=modele.make_future_dataframe(periods=n_periods)
+        prediction=model.predict(future_dates)
+        elif price_type=='Open':
+          #Open Model
+        '''  df_open=data['Open']
+          df_open.index.rename('ds',True)
+          df_open=df_open.reset_index()
+          df_open.columns=['ds','y']
+          model_open=Prophet()
+          model_open.fit(df_open)
+          future_dates_open=model_open.make_future_dataframe(periods=n_periods)
+          prediction_open=model_open.predict(future_dates_open)
+        elif price_type=='High'
+          #High Model
+          df_high=data['High']
+          df_high.index.rename('ds',True)
+          df_high=df_high.reset_index()
+          df_high.columns=['ds','y']
+          model_high=Prophet()
+          model_high.fit(df_high)
+          future_dates_high=model_high.make_future_dataframe(periods=n_periods)
+          prediction_high=model_high.predict(future_dates_high)
+        elif price_type=='Low':
+          #Low Model
+          df_low=data['Low']
+          df_low.index.rename('ds',True)
+          df_low=df_low.reset_index()
+          df_low.columns=['ds','y']
+          model_low=Prophet()
+          model_low.fit(df_low)
+          future_dates_low=model_low.make_future_dataframe(periods=n_periods)
+          prediction_low=model_low.predict(future_dates_low)
         fig = go.Figure(data=[go.Candlestick(x=new_index,
                     open=prediction_open.yhat,
                     high=prediction_high.yhat,
                     low=prediction_low.yhat,
-                    close=prediction_close.yhat)])
+                    close=prediction_close.yhat)])'''
            
-        st.plotly_chart(fig)
-        return model_close,prediction_close  
-    model_close,prediction_close=forecast(combined_data[stock])    
-    second_graph=st.sidebar.checkbox('Forecast v.s Actual Plot')  
-    if second_graph:
-        fig=plot_plotly(model_close,prediction_close,trend=True)
-        st.plotly_chart(fig)
+        #st.plotly_chart(fig)
+        return model,prediction  
+    model,prediction=forecast(combined_data[stock],price_type)    
+    fig=plot_plotly(model,prediction,trend=True)
+    st.plotly_chart(fig)
+        
     
 #Simple Moving Average
 sma = st.sidebar.checkbox('Simple Moving Average')
