b79a37cf75dd4216d8eaffef1fb8e0d2b35525e8
ScarceDS <75695610+ScarceDS@users.noreply.github.com>
2021-07-12 18:29:28 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -202,13 +202,15 @@ if Forecasting:
         
         model=Prophet(changepoint_prior_scale=changepoint_prior_scale,seasonality_mode=seasonality_mode,seasonality_prior_scale=seasonality_prior_scale).add_country_holidays(country_name='US').fit(df)
         
+        st.sidebar.txt('Model Training is completed')
+        st.sidebar.header('Cross Validation Customization')
         Training_size=st.sidebar.number_input('Training Set Size in Days',value=int(0.45*len(df)),min_value=int(0.25*len(df)),max_value=int(0.75*len(df)))
-        cutoff_seperation=st.sidebar.number_input('OFF period Size in Days',value=int(0.1*len(df)),min_value=int(0.05*len(df)),max_value=int(0.5*len(df)))
+        cutoff_seperation=st.sidebar.number_input('Rolling Window Size in Days',value=int(0.1*len(df)),min_value=int(0.05*len(df)),max_value=int(0.5*len(df)))
         Validation_size=st.sidebar.number_input('Validation/Forecasting Set Size in Days',value=int(0.05*len(df)),min_value=int(0.05*len(df)),max_value=int(0.2*len(df)))
         
         #cutoffs = pd.to_datetime([df['ds'][int(0.55*len(df))],df['ds'][int(0.75*len(df))]])
         df_cv = cross_validation(model,initial=str(Training_size)+' days',period=str(cutoff_seperation)+' days', horizon=str(Validation_size)+' days', parallel=None)
-        df_cv = cross_validation(model, cutoffs=cutoffs, horizon='30 days', parallel="processes")
+        #df_cv = cross_validation(model, cutoffs=cutoffs, horizon='30 days', parallel="processes")
         df_p = performance_metrics(df_cv, rolling_window=1)
         st.sidebar.subheader(f"RMSE =\n {df_p['rmse'].mean()}")
         future_dates=model.make_future_dataframe(periods=n_periods)
